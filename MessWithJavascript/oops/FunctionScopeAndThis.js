b743596f62f2fb91f0500d5e9ee6aee194e1981f
Sundeep Sivaraman <sundeep.sivaraman@gmail.com>
2014-07-21 22:45:01 -0700

    Applied Eclipse Formatting

--- MessWithJavascript/oops/FunctionScopeAndThis.js
+++ MessWithJavascript/oops/FunctionScopeAndThis.js
@@ -1,38 +1,37 @@
 /*
 NOTES on Function, scope and "this":
-* There is nothing like block-scope in Javascript. There is only a function-scope.
-* All variables defined in function are visible throughout the function and are hoisted.
+ * There is nothing like block-scope in Javascript. There is only a function-scope.
+ * All variables defined in function are visible throughout the function and are hoisted.
 It is good practice to define all the variables at the top of the function.
-* The value of "this" changes according to the way the function is used [i.e. whether
+ * The value of "this" changes according to the way the function is used [i.e. whether
 it is used as regular function, method or constructor]
-* When used as regular function, this points to the Global object [Window in case of
+ * When used as regular function, this points to the Global object [Window in case of
 the web-browser]
-* When used as an object-method, this refers to the object containing the method.
-* When used as a Constructor, this refers to the object that is being returned
+ * When used as an object-method, this refers to the object containing the method.
+ * When used as a Constructor, this refers to the object that is being returned
 from the constructor
-*/
+ */
 
 // Cat is a Constructor function for creating new cats
-function Cat(name){
-  this.name = name;
-  console.log(this); // 'this' refers to the Cat object that is being created
+function Cat(name) {
+	this.name = name;
+	console.log(this); // 'this' refers to the Cat object that is being created
 }
 
-Cat.prototype.talk = function(){
-  console.log(this.name + " says Meooow");
+Cat.prototype.talk = function() {
+	console.log(this.name + " says Meooow");
 };
-
 
 // newCat is a factory function for creating new cats.
 function newCat(name) {
-  console.log(this); // this refers to the global-object i.e. Window
-  return {
-    name: name,
-    talk: function() {
-	    	console.log(this); // this refers to the object containing the "talk" method
-	    	console.log(this.name + " says Meoow");
-    	  },
-   };
+	console.log(this); // this refers to the global-object i.e. Window
+	return {
+		name : name,
+		talk : function() {
+			console.log(this); // this refers to the object containing the "talk" method
+			console.log(this.name + " says Meoow");
+		},
+	};
 }
 
 // Instantiate objects using the above two approaches:
